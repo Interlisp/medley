c0564a85f33d367d274f0174aa4294cee468e971
Larry Masinter <LMM@acm.org>
2021-03-08 20:01:44 -0800

    Main cleanup of new init process, scripts, etc

--- scripts/loadup-mid-from-init.sh
+++ scripts/loadup-mid-from-init.sh
@@ -10,7 +10,7 @@ fi
 mkdir -p "$MEDLEYDIR/tmp"
 scr="-sc 1024x768 -g 1042x790"
 
-./run-medley -prog ldeinit -greet $MEDLEYDIR/sources/XREM.CM $scr -vmem tmp/init-mid.sysout loadups/init.dlinit
+./run-medley -prog ldeinit -greet $MEDLEYDIR/sources/XREM.CM $scr -vmem tmp/init-mid.sysout tmp/init.dlinit
 
 echo ---- made ----
 ls -l tmp/

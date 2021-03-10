4119b2a68034cabf2b48128c4b6f1bd44ba09db4
Larry Masinter <lmm@acm.org>
2021-03-09 17:25:13 -0800

    Make exports.all and whereis.hash in tmp directory. Add script to release. (#249)

---
+++ scripts/release-all.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export MEDLEYDIR=`pwd`
+
+if [ ! -x run-medley ] ; then
+    echo run from MEDLEYDIR
+    exit 1
+fi
+
+cp -p tmp/full.sysout tmp/lisp.sysout tmp/*.dribble tmp/whereis.hash loadups/
+cp -p tmp/exports.all library/
+

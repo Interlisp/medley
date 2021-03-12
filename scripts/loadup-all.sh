6709890fbe63325a6267905e3e8d4d1575a8dbb1
Larry Masinter <LMM@acm.org>
2021-03-12 12:45:10 -0800

    brute force: close open files when done loadup

--- scripts/loadup-all.sh
+++ scripts/loadup-all.sh
@@ -7,6 +7,10 @@ if [ ! -x run-medley ] ; then
     exit 1
 fi
 
-./scripts/loadup-init.sh && ./scripts/loadup-mid-from-init.sh && ./scripts/loadup-lisp-from-mid.sh && ./scripts/loadup-full.sh
+./scripts/loadup-init.sh && \
+    ./scripts/loadup-mid-from-init.sh && \
+    ./scripts/loadup-lisp-from-mid.sh && \
+    ./scripts/loadup-full.sh && \
+    ./scripts/loadup-aux.sh
 
 echo tada!

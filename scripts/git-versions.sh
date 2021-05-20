3644c17dc991441f6fea4b1a88f84dfe841874bf
Larry Masinter <LMM@acm.org>
2021-05-19 22:05:59 -0700

    remove tracing

--- scripts/git-versions.sh
+++ scripts/git-versions.sh
@@ -1,13 +1,12 @@
-#!/bin/sh -vx
+#!/bin/sh
 
 #missing features:
-#  should take a directory and work for all of the files
-#  deletes any existing versions !!!
-#   then checks out any file.~nn~ files
-#   maybe if they are checked in, then start at nn+1 
-#  should check to see if file hasn't been committed
-#    stash it first, check out previous versions, pop the stash
-#  should never make hardlink if there is only 1
+# [ ] should take a directory and work for all of the files
+# [*] deletes any existing versions !!!
+#       then checks out any file.~nn~ files
+# [ ] should check to see if file hasn't been committed
+#     stash it first, check out previous versions, pop the stash
+# [ ] should never make hardlink if there is only 1
 #  Maybe only do this for Lisp sources (and LCOMs? TEdit files? Not sysouts?)
 #  should coordinate with fix-links and trimming repo size
 
@@ -27,19 +26,32 @@ rm -f "$file".~[1-9]*~
 
 n=1
 
-for commit in `git log --remove-empty --reverse --format="%h" --  "$file.~[1-9]*~"` 
+# first find commits with explicit veeersions
+for commit in `git log --remove-empty --reverse --format="%h" -- "$file.~[1-9]*~"` 
 do git checkout $commit "$file.~[1-9]*~"
    for version in "$file".~[1-9]*~
    do
        vn=`echo $version | grep --only-matching '\\.~[1-9][0-9]*~' | grep --only-matching '[1-9][0-9]*'`
        if [ $vn -ge $n ]; then
+	   # update $n to version $vn
 	   n=`expr $vn + 1`
        fi
    done
 done
-   
+
 for commit in `git log --remove-empty --reverse --format="%h" "$file"`
-do git checkout $commit "$file" && ln "$file" "$file.~"$n"~" && n=`expr $n + 1`
+do git checkout $commit "$file"
+   fcv=`tr '\r' '\n' <$file | tr -d '\001-\006' | head -n 2 | grep --only-matching '\\.;[1-9][0-9]* ' | grep --only-matching '[1=9][0-9]*'`
+   if [ ! -z $fcv ]; then
+       if [ $fcv -gt $n ]; then
+	   echo update $n to $fcv version
+	   n=`expr $fcv`
+       else
+	   # $n not updated to $fcv file version
+       fi
+       # no version found in $file $n
+   fi
+   ln "$file" "$file.~"$n"~" && n=`expr $n + 1`
 done
 
 if [ $n -eq 2 ]; then

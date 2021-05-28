bf64f83b26a38fb578763aba5c8fb1f6dd0c77dc
Larry Masinter <LMM@acm.org>
2021-05-28 12:02:07 -0700

    Update to set checkout version to be original file version if possible

--- scripts/git-versions.sh
+++ scripts/git-versions.sh
@@ -2,13 +2,14 @@
 
 #missing features:
 # [ ] should take a directory and work for all of the files
-# [*] deletes any existing versions !!!
+# [*] deletes any existing versions Is that OK???
 #       then checks out any file.~nn~ files
 # [ ] should check to see if file hasn't been committed
-#     stash it first, check out previous versions, pop the stash
-# [ ] should never make hardlink if there is only 1
-#  Maybe only do this for Lisp sources (and LCOMs? TEdit files? Not sysouts?)
-#  should coordinate with fix-links and trimming repo size
+#     if so, it should stash it first, check out previous versions, pop the stash
+#  should coordinate with medley-fix-links and trimming repo size
+
+# only ASCII
+export LC_ALL=C 
 
 file="$1"
 
@@ -22,38 +23,46 @@ if [ ! -f "$file" ]; then
     exit 1
 fi
 
+base=`basename $file .LCOM`
+base=`basename $base .DFASL`
+
 rm -f "$file".~[1-9]*~
 
 n=1
 
-# first find commits with explicit veeersions
+# first find commits with explicit versions
 for commit in `git log --remove-empty --reverse --format="%h" -- "$file.~[1-9]*~"` 
-do git checkout $commit "$file.~[1-9]*~"
-   for version in "$file".~[1-9]*~
-   do
-       vn=`echo $version | grep --only-matching '\\.~[1-9][0-9]*~' | grep --only-matching '[1-9][0-9]*'`
-       if [ $vn -ge $n ]; then
-	   # update $n to version $vn
-	   n=`expr $vn + 1`
-       fi
-   done
+do git checkout -q $commit "$file.~[1-9]*~" 2>/dev/null
+done
+# should only be one commit but just in case
+
+for version in "$file".~[1-9]*~
+do  vn=`echo $version | grep --only-matching '\.~[1-9][0-9]*~' | grep --only-matching '[1-9][0-9]*'`
+    vn=`expr $vn + 1`  
+    if [ $n -lt $vn ]; then
+	n=$vn
+    fi
 done
 
 for commit in `git log --remove-empty --reverse --format="%h" "$file"`
-do git checkout $commit "$file"
-   fcv=`tr '\r' '\n' <$file | tr -d '\001-\006' | head -n 2 | grep --only-matching '\\.;[1-9][0-9]* ' | grep --only-matching '[1=9][0-9]*'`
+do git checkout -q $commit "$file"
+   fcv=`tr '\r' '\n' <"$file" | tr -d '\001-\006' | head -n 2 | grep -a --only-matching '{DSK}.*'$base'\.\?;[1-9][0-9]*'  | grep --only-matching ';[1-9][0-9]*'`
+   fcv=`echo $fcv | grep --only-matching '[1-9][0-9]*'`
    if [ ! -z $fcv ]; then
        if [ $fcv -gt $n ]; then
-	   echo update $n to $fcv version
-	   n=`expr $fcv`
-       else
-	   # $n not updated to $fcv file version
+	   n=$fcv
        fi
-       # no version found in $file $n
    fi
    ln "$file" "$file.~"$n"~" && n=`expr $n + 1`
 done
 
+
 if [ $n -eq 2 ]; then
-   rm -f "$file".~1~
+    rm -f "$file".~1~
 fi
+
+git restore --staged "$file.~[1-9]*~" 2>/dev/null    
+
+
+
+

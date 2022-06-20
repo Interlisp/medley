c688808306a855b2d6dd3ecdc0cb5d3272c72da0
Larry Masinter <LMM@acm.org>
2022-06-19 22:28:51 -0700

    loadup-db.sh - script to load as much as possible and Masterscope everything. Make 'fuller.database' and 'fuller.sysout'

---
+++ scripts/loadup-db.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -x
+
+export MEDLEYDIR=`pwd`
+
+if [ ! -f run-medley ] ; then
+    echo run from MEDLEYDIR
+    exit 1
+fi
+
+touch tmp/loadup.timestamp
+
+# Keep (GREET) from finding an init file
+mkdir -p $MEDLEYDIR/tmp/logindir
+export HOME=$MEDLEYDIR/tmp/logindir
+export LOGINDIR=$MEDLEYDIR/tmp/logindir
+
+scr="-sc 1024x768 -g 1042x790"
+
+echo '" (IL:MEDLEY-INIT-VARS)(IL:FILESLOAD MEDLEY-UTILS)(IL:MAKE-FULLER-DB)(IL:LOGOUT)"' > tmp/loadup-db.cm
+./run-medley $scr -loadup "$MEDLEYDIR"/tmp/loadup-db.cm tmp/full.sysout
+
+if [ tmp/full.database -nt tmp/loadup.timestamp ]; then
+    
+    echo ---- made ----
+    ls -l tmp/fuller*
+    echo --------------
+
+else
+    echo XXXXX FAILURE XXXXX
+    ls -l tmp/fuller*
+    exit 1
+fi

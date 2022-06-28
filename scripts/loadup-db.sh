32128f5e19ebb411c7952a7eafcbefc0f9d41fd3
Larry Masinter <lmm@acm.org>
2022-06-28 11:45:59 -0700

    MEDLEY-UILS loadup-db run-medley fixes (#808)
    * MEDLEY-UILS loadup-db run-medley fixes
    
    * UNIXCOMM compile to DFASL; only set UTF-8 if getenv(LANG). loadup-db no lisp.virtualmem

---
+++ scripts/loadup-db.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+export MEDLEYDIR=`pwd`
+
+if [ ! -f run-medley ] ; then
+    echo run from MEDLEYDIR
+    exit 1
+fi
+
+touch tmp/loadup.timestamp
+
+# Keep (GREET) from finding an init file
+mkdir -p $MEDLEYDIR/tmp/logindir
+export HOME=$MEDLEYDIR/tmp/logindir
+export LOGINDIR=$MEDLEYDIR/tmp/logindir
+
+scr="-sc 1024x768 -g 1042x790"
+
+echo '" (IL:MEDLEY-INIT-VARS)(IL:FILESLOAD MEDLEY-UTILS)(IL:MAKE-FULLER-DB)(IL:LOGOUT T)"' > tmp/loadup-db.cm
+./run-medley $scr -loadup "$MEDLEYDIR"/tmp/loadup-db.cm tmp/full.sysout
+
+if [ tmp/fuller.database -nt tmp/loadup.timestamp ]; then
+    
+    echo ---- made ----
+    ls -l tmp/fuller*
+    echo --------------
+
+else
+    echo XXXXX FAILURE XXXXX
+    ls -l tmp/fuller*
+    exit 1
+fi

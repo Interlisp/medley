7eaac5be63b3b4f851451e1e83439a293e294281
Larry Masinter <LMM@acm.org>
2022-10-24 22:54:40 -0700

    add copy-all.sh at end of loadup-all.sh to eliminate fialing tmp/ hackery

--- scripts/loadup-all.sh
+++ scripts/loadup-all.sh
@@ -11,12 +11,9 @@ fi
     ./scripts/loadup-mid-from-init.sh && \
     ./scripts/loadup-lisp-from-mid.sh && \
     ./scripts/loadup-full-from-lisp.sh && \
-    ./scripts/loadup-aux.sh
+    ./scripts/loadup-aux.sh && \
+    ./scripts/copy-all.sh
 
-echo "loadups are in $MEDLEYDIR/tmp"
-echo use
-echo "   ./scripts/copy-all.sh   "
-echo "to copy to loadups library"
 echo "**** DONE ****"
 
 

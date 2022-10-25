d5d21397d48bf263d13d64beb97d69b99cc94b1a
Larry Masinter <lmm@acm.org>
2022-10-25 14:43:57 -0700

    another pass at variable initialization after logout savevm sysout makesys (#1003)
    This corrects some errors in the handling of initializing variables across SAVEVM, LOGOUT, SYSOUT and MAKESYS.
    This is all now handled by MEDLEY-INIT-VARS (function and variable) which is called as an EVENTFN.
    BEFOREMAKESYS (invoked by ENDLOADUP) clears the variables to a default setting (all directories are just {DSK}).
    The other "BEFORE" events save away the current values of the variables in MEDLEY-INIT-VARS.
    
    In order to get this to work it was necessary to change a hack for deciding where to find EXPORTS.ALL and WHEREIS.HASH. Now  if you do `./scripts/loadup-all.sh` to make a full, lisp sysouts, exports.all and whereis.hash it will still build the sysouts in tmp/ but will also "link" new versions in loadups (and library for exports.all). This replaces the previous hack scanning the sysout name for "tmp/".
    MEDLEY-INIT-VARS had been called both by the AROUNDEXITFN and AFTER*FORMS.

--- scripts/loadup-db.sh
+++ scripts/loadup-db.sh
@@ -7,14 +7,14 @@ if [ ! -f run-medley ] ; then
     exit 1
 fi
 
-touch tmp/loadup.timestamp
+touch tmp/db.timestamp
 
 scr="-sc 1024x768 -g 1042x790"
 
-echo '" (IL:MEDLEY-INIT-VARS)(IL:FILESLOAD MEDLEY-UTILS)(IL:MAKE-FULLER-DB)(IL:LOGOUT T)"' > tmp/loadup-db.cm
-./run-medley $scr -loadup "$MEDLEYDIR"/tmp/loadup-db.cm tmp/full.sysout
+echo '" (IL:MEDLEY-INIT-VARS)(IL:FILESLOAD MEDLEY-UTILS)(IL:MAKE-FULLER-DB)(IL:LOGOUT T)"' > tmp/db.cm
+./run-medley $scr -loadup "$MEDLEYDIR"/tmp/db.cm -full
 
-if [ tmp/fuller.database -nt tmp/loadup.timestamp ]; then
+if [ tmp/fuller.database -nt tmp/db.timestamp ]; then
     
     echo ---- made ----
     ls -l tmp/fuller*

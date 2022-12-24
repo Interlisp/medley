4ae16aad55cb8126c045d989b9b2719fad687ab1
Larry Masinter <LMM@acm.org>
2022-12-23 16:20:06 -0800

    loadup-db.sh fix -- don't rely on loadups

--- scripts/loadup-db.sh
+++ scripts/loadup-db.sh
@@ -12,8 +12,7 @@ touch tmp/db.timestamp
 scr="-sc 1024x768 -g 1042x790"
 
 echo '" (IL:MEDLEY-INIT-VARS)(IL:FILESLOAD MEDLEY-UTILS)(IL:MAKE-FULLER-DB)(IL:LOGOUT T)"' > tmp/db.cm
-./run-medley $scr -loadup "$MEDLEYDIR"/tmp/db.cm -full
-
+./run-medley $scr -loadup "$MEDLEYDIR"/tmp/db.cm -n
 if [ tmp/fuller.database -nt tmp/db.timestamp ]; then
     
     echo ---- made ----

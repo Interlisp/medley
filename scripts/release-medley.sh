84391b367b4482ce45540e0c2921a00340874faf
Frank Halasz <frank@halasz.org>
2023-08-02 16:43:35 -0700

    First pass a complete new make-release.sh

--- scripts/release-medley.sh
+++ scripts/release-medley.sh
@@ -6,6 +6,24 @@ if [ ! -x run-medley ] ; then
     exit 1
 fi
 
+if [ -z "$(which gh)" ]; then
+    echo "Error: this script requires gh (github cli) to be installed. Exiting."
+    exit 1
+fi
+
+gh auth status 2>&1 | grep -i -q "Logged in to github.com"
+if [ $? -ne 0 ]; then
+    echo 'Error: this script requires you to be logged into github.  Use "gh auth login" to do so. Exiting.'
+    exit 1
+fi
+
+if [ "$1" = "-d" ]; then
+    draft="-d"
+    shift
+else
+    draft=""
+fi
+
 if [ -z "$1" ] ; then
     tag=medley-$(date +%y%m%d)-$(date +%s)
 else
@@ -17,10 +35,10 @@ scripts/release-make-tars.sh "${tag}"
 
 echo making release
 sed s/'$tag'/$tag/g < release-notes.md > releases/${short_tag}/release-notes.md
-gh release create $tag -F releases/${short_tag}/release-notes.md -p -t $tag
+gh release create $tag -F releases/${short_tag}/release-notes.md -p -t $tag ${draft}
 
 echo uploading
 gh release upload $tag releases/${short_tag}/$tag-loadups.tgz releases/${short_tag}/$tag-runtime.tgz --clobber
 
-echo done
+echo "Done with release ${tag}"
 

b8b2a0cd65d34d5e1abe372a07a93726e9e657ac
Nick Briggs <nicholas.h.briggs@gmail.com>
2025-01-25 11:21:51 -0800

    Revert "Add a call to check for orphaned versions after any checkout (#1973)"
    This reverts commit 9f980276bf654e40cc2dbc50f8cbaaa46d127460.

--- scripts/find-orphaned-versions.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-# $1 is a versioned file name
-
-ver="$1"
-base="${ver%%.~[1-9]*~}"
-if [ ! -f "$base" ]; then 
-    echo "Orphaned version: $ver but no $base"
-fi
-
--- scripts/post-checkout.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-
-find . -name "*.~[1-9]*~" -exec if \[ ! -f {}:h \]\; then echo "{}" " with no versionless" \;
